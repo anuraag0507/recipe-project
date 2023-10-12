37af52a47bf4a1e826e3f3cbf023ec3bf635f01c
anuraag0507 <93443023+anuraag0507@users.noreply.github.com>
2023-10-12 23:20:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import Tabs from './components/Tabs'
  import RecipeLists from './components/RecipeLists'
 import { useState } from 'react';
-
+ 
 function App() {
   const [loader,setLoader] = useState(true)
   return (
